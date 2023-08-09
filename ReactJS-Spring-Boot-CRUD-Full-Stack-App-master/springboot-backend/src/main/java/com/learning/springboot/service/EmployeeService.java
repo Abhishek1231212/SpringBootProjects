f53b70596c9d5e13422cfca01ec76804211efdfb
Abhishek <bichalaabhishek@gmail.com>
2023-08-09 16:39:32 +0530

    Added Full Stack Project and some updates for previous project

---
+++ ReactJS-Spring-Boot-CRUD-Full-Stack-App-master/springboot-backend/src/main/java/com/learning/springboot/service/EmployeeService.java
@@ -0,0 +1,54 @@
+package com.learning.springboot.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import com.learning.springboot.exception.ResourceNotFoundException;
+import com.learning.springboot.model.Employee;
+import com.learning.springboot.repository.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
+
+	public Employee createEmployee(Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	public ResponseEntity<Employee> getEmployeeById(Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+		return ResponseEntity.ok(employee);
+	}
+
+	public ResponseEntity<Employee> updateEmployee(Long id, Employee employeeDetails) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmailId(employeeDetails.getEmailId());
+
+		Employee updatedEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);
+	}
+
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id :" + id));
+
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+}
